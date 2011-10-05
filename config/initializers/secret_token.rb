59d082a83313a088397886a58ff22e2170b55579
Ty Barho <t.barho@gmail.com>
2011-10-05 19:10:49 +0000

    First commit.  Added authentication & pages from ty-gen

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moaz::Application.config.secret_token = '4bf02bcdd79dfa9ad303e0197b0be4462adb13a6a468a15be693d2fd4af2c686859eff54283d8c622d2437da5a6d817a4f99cdc5934807e1be03143ac6b36da1'
