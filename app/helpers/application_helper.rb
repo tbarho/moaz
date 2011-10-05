a1c1fe1619a1ed4a07c54d2b1db779d738313b21
Ty Barho <t.barho@gmail.com>
2011-10-05 20:53:50 +0000

    Added layout files, got some basic CSS working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def logo
+    image_tag("logo.gif", :alt => "Moazami Homes")
+  end
+
   # Return a title on a per-page basis
   def title
     base_title = "My New App"
