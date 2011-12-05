2cc0852dc820bf6bf338782afdea0b5d7d1a2967
Ty Barho <t.barho@gmail.com>
2011-12-05 22:35:36 +0000

    Changed website default title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
 
   # Return a title on a per-page basis
   def title
-    base_title = "My New App"
+    base_title = "Moazami Homes"
     if @title.nil?
       base_title
     else
